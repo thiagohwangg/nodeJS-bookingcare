b2fe9ea6ab7c7301676d9d2d72b1599b594810fa
thien.hoang <thien.hoang@s3corp.com.vn>
2024-03-29 17:46:19 +0700

    lesson65

--- src/migrations/migration-create-clinic.js
+++ src/migrations/migration-create-clinic.js
@@ -20,6 +20,10 @@ module.exports = {
       image: {
         type: Sequelize.STRING,
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
---
+++ src/migrations/migration-markdown.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// contentHTML: DataTypes.,
+//       : ,
+//       description: DataTypes.,
+//       : DataTypes.,
+//       : DataTypes.INTEGER,
+//       : DataTypes.INTEGER,
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("markdown", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      contentHTML: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      contentMarkdown: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.TEXT("long"),
+      },
+      doctorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      specialtyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      clinicId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("markdown");
+  },
+};
