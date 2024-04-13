6ee85870522bb83cb699c51fd91351ad72c011ad
thiagohwangg <cmnqpart2@gmail.com>
2024-04-14 00:01:37 +0700

    lesson75

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -57,10 +57,38 @@ let getDetailDoctorById = async(req, res) => {
   }
 }
 
+let bulkCreateSchedule = async(req, res) => {
+  try {
+    let infor = await doctorService.bulkCreateSchedule(req.body)
+    return res.status(200).json(infor)
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: 'Error from the server'
+    })
+  }
+}
+
+let getScheduleByDate = async(req, res) => {
+  try {
+    let infor = await doctorService.getScheduleByDate(req.query.doctorId, req.query.date)
+    return res.status(200).json(infor)
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: 'Error from the server'
+    })
+  }
+}
+
 
 module.exports = {
   getTopDoctorHome,
   getAllDoctors,
   postInfoDoctor,
-  getDetailDoctorById
+  getDetailDoctorById,
+  bulkCreateSchedule,
+  getScheduleByDate
 };
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,9 @@
-const { raw } = require("body-parser");
 const db = require("../models");
+require("dotenv").config();
+import { raw } from "body-parser";
+import _, { reject } from "lodash";
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
 let getTopDoctorHome = (limitInput) => {
   return new Promise(async (resolve, reject) => {
@@ -87,7 +91,7 @@ let saveDetailInfoDoctor = (inputData) => {
             doctorMarkdown.contentHTML = inputData.contentHTML;
             doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
             doctorMarkdown.description = inputData.description;
-            doctorMarkdown.updatedAt = new Date()
+            doctorMarkdown.updatedAt = new Date();
 
             await doctorMarkdown.save();
           }
@@ -152,9 +156,92 @@ let getDetailDoctorById = (inputId) => {
   });
 };
 
+let bulkCreateSchedule = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameter!",
+        });
+      } else {
+        let schedule = data.arrSchedule;
+        if (schedule && schedule.length > 0) {
+          schedule = schedule.map((item) => {
+            item.maxNumber = MAX_NUMBER_SCHEDULE;
+            return item;
+          });
+        }
+        // get all existing date
+        let existing = await db.Schedule.findAll({
+          where: { doctorId: data.doctorId, date: data.formatedDate },
+          attributes: ["timeType", "date", "doctorId", "maxNumber"],
+          raw: true
+        });
+
+        // convert date
+        if (existing && existing.length > 0) {
+          existing = existing.map((item) => {
+            item.date = new Date(item.date).getTime();
+            return item;
+          });
+        }
+
+        // compare different 
+        let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+          return a.timeType === b.timeType && a.date === b.date;
+        });
+
+        // create data
+        if(toCreate && toCreate.length > 0) {
+          await db.Schedule.bulkCreate(toCreate);
+        }
+        resolve({
+          errCode: 0,
+          errMessage: "OK",
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let getScheduleByDate = (doctorId, date) => {
+  return new Promise(async(resolve, reject) => {
+    try {
+      if(!doctorId || !date) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Missing required parameters!'
+        })
+      } else {
+        let dataSchedule = await db.Schedule.findAll({
+          where: {
+            doctorId: doctorId,
+            date: date
+          }
+        })
+
+        if(!dataSchedule) dataSchedule = [];
+
+        resolve({
+          errCode: 0,
+          data: dataSchedule
+        })
+      }
+    } catch (error) {
+      reject(error)
+      
+    }
+  })
+}
+
 module.exports = {
   getTopDoctorHome,
   getAllDoctors,
   saveDetailInfoDoctor,
   getDetailDoctorById,
+  bulkCreateSchedule,
+  getScheduleByDate
 };
