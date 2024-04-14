90e23abfc00e4b62a75f344f448578328dc93a0a
thiagohwangg <cmnqpart2@gmail.com>
2024-04-15 00:14:49 +0700

    lesson76

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -178,18 +178,10 @@ let bulkCreateSchedule = (data) => {
           attributes: ["timeType", "date", "doctorId", "maxNumber"],
           raw: true
         });
-
-        // convert date
-        if (existing && existing.length > 0) {
-          existing = existing.map((item) => {
-            item.date = new Date(item.date).getTime();
-            return item;
-          });
-        }
-
+        
         // compare different 
         let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-          return a.timeType === b.timeType && a.date === b.date;
+          return a.timeType === b.timeType && +a.date === +b.date;
         });
 
         // create data
@@ -219,8 +211,17 @@ let getScheduleByDate = (doctorId, date) => {
         let dataSchedule = await db.Schedule.findAll({
           where: {
             doctorId: doctorId,
-            date: date
-          }
+            date: date,
+          },
+          include: [
+            {
+              model: db.Allcode,
+              as: "timeTypeData",
+              attributes: ["valueEn", "valueVi"],
+            },
+          ],
+          raw: false,
+          nest: true,
         })
 
         if(!dataSchedule) dataSchedule = [];
