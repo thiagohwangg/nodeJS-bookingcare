05ff32615fed64885e74a961401f436507b55588
thiagohwangg <cmnqpart2@gmail.com>
2024-04-29 22:12:46 +0700

    lesson91

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -13,6 +13,20 @@ let createSpecialty = async(req, res) => {
       }
 }
 
+let getAllSpecialty = async(req, res) => {
+  try {
+      let infor = await specialtyService.getAllSpecialty()
+      return res.status(200).json(infor)
+    } catch (error) {
+      console.log("error: ", error);
+      return res.status(200).json({
+        errCode: -1,
+        errMessage: 'Error from the server'
+      })
+    }
+}
+
 module.exports = {
-    createSpecialty
+    createSpecialty,
+    getAllSpecialty,
 }
